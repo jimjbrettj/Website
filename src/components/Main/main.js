1f56caa7892edc2c732214fce1f8ea53aa1f5c6d
jjohnson <jimjbrettj@yahoo.com>
2019-07-11 11:25:25 -0400

    Initial state

---
+++ src/components/Main/main.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './main.css';
+
+function main() {
+  return (
+   <div className="back">
+     <div className="head">Hey Griff lets get this bread!</div>
+   </div>
+  );
+}
+
+export default main;
